8296d8fc4435a0b9f3c2fc8bf45f4e70aecde28c
Aaron Haley <15240680+mechdude@users.noreply.github.com>
2018-01-14 21:48:30 -0600

    add test for toJs relationships

--- __tests__/toJs.spec.js
+++ __tests__/toJs.spec.js
@@ -11,3 +11,24 @@ test('toJs converts empty AML to empty Item list', () =>{
 test('toJs preserves Item list', () =>{
     expect(toJs('<AML><Item/><Item/></AML>')).toEqual({'AML': {'Item': [ {}, {} ]}})
 })
+
+test('toJs converts empty Relationships to empty Item list', () =>{
+    expect(toJs('<AML><Item><Relationships></Relationships></Item></AML>')).toEqual(
+        {AML: {Item: [
+            {Relationships: {Item: [ ]} }
+        ]}})
+})
+
+test('toJs converts single Item in Relationships to list', () =>{
+    expect(toJs('<AML><Item><Relationships><Item/></Relationships></Item></AML>')).toEqual(
+        {AML: {Item: [
+            {Relationships: {Item: [ {} ]} }
+        ]}})
+})
+
+test('toJs preserves Item list in Relationships', () =>{
+    expect(toJs('<AML><Item><Relationships><Item/><Item/></Relationships></Item></AML>')).toEqual(
+        {AML: {Item: [
+                {Relationships: {Item: [ {}, {} ]} }
+        ]}})
+})
